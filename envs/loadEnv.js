c60305ddb73dc2881b3dfbe511906322af17f828
李昀宸 <b09705015@ntu.edu.tw>
2024-02-14 12:49:23 +0800

    feat(app): initialize environment

---
+++ envs/loadEnv.js
@@ -0,0 +1,18 @@
+const dotEnv = require("dotenv");
+const { resolve } = require("node:path");
+
+const dotEnvFileMap = {
+  development: "development",
+  production: "production",
+};
+
+/** @type {(NODE_ENV:"development" | "production") => void} */
+function loadEnv(NODE_ENV) {
+  const mode = dotEnvFileMap[NODE_ENV || "development"];
+  const envFileList = [`.${mode}.local`, ".local", `.${mode}`, ""];
+  envFileList?.forEach((envFile) =>
+    dotEnv.config({ path: resolve(__dirname, `./.env${envFile}`) })
+  );
+}
+
+module.exports = loadEnv;
