ad7c4d0c8066ccae27ded18624606ff2b4f4691f
李昀宸 <b09705015@ntu.edu.tw>
2024-03-19 15:35:17 +0800

    feat(app): initialize eslint & husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+import type { Metadata } from "next";
+
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
